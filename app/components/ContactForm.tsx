87392b564161aa2f41ee1029f387e2d0303d1acb
shimaaaaaaa <island.hope123@gmail.com>
2025-11-27 18:28:55 +0900

    add forms (pending)

---
+++ app/components/ContactForm.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function ContactForm() {
+  return (
+    <form action="">
+      <input type="text" />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
---
+++ app/contacts/page.tsx
@@ -0,0 +1,9 @@
+import ContactForm from "../components/ContactForm";
+
+export default function ContactForm() {
+  return (
+    <div>
+      <ContactForm />
+    </div>
+  );
+}
