f00c4edecba257958b055cd4ccf1efa7a45bdfce
shimaaaaaaa <island.hope123@gmail.com>
2025-12-10 23:47:51 +0900

    add validation func

--- app/lib/actions/contact.ts
+++ app/lib/actions/contact.ts
@@ -1,10 +1,17 @@
 "use server";
 import { redirect } from "next/navigation";
+import { ContactSchema } from "./validations/contacts";
 
 export default async function contact(formData: FormData) {
   const name = formData.get("name");
   const email = formData.get("email");
   const message = formData.get("message");
+  const validationResult = ContactSchema.safeParse({ name, email });
+  if (!validationResult.success) {
+    const errors = validationResult.error.flatten();
+    console.log("ERROR: ", errors);
+    return { success: false, errors: errors.fieldErrors };
+  }
   console.log(name, email, message);
   redirect("/contacts/complete");
 }
