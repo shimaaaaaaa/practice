03fd076374ff3aefd4433c702d7f28e81cc0d433
shimaaaaaaa <island.hope123@gmail.com>
2025-12-15 23:36:39 +0900

    move validations folder

--- app/lib/actions/contact.ts
+++ app/lib/actions/contact.ts
@@ -1,6 +1,6 @@
 "use server";
 import { redirect } from "next/navigation";
-import { ContactSchema } from "./validations/contacts";
+import { ContactSchema } from "../validations/contacts";
 
 type ActionState = {
   success: boolean;
